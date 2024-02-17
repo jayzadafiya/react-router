6c89724abe237d14c4fb8d999dcbd64a51eeb573
jay zadafiya <zadafiyajay2@gmail.com>
2024-02-17 11:03:20 +0530

    managing the token Expiration

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import { Outlet, useNavigate, useNavigation } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLoaderData, useNavigate, useNavigation, useSubmit } from 'react-router-dom'
 import MainNavigation from '../components/MainNavigation'
+import { getTokenDuration } from '../util/auth';
 
 function Root() {
     // const navigation = useNavigation();
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+
+        if (!token) {
+            return;
+        }
+
+        if (token === 'EXPIRED') {
+            submit(null, { action: '/logout', method1: 'post' })
+        }
+
+        const tokenDuration = getTokenDuration();
+
+        setTimeout(() => {
+            submit(null, { action: '/logout', method1: 'post' })
+        }, tokenDuration);
+
+    }, [token, submit])
 
     return (
         <>
